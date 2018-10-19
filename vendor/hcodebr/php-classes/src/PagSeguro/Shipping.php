6033f188f7b8fb76984dd4ad54f40fe160985e58
Estevam Morais <estevamferretti@gmail.com>
2018-10-19 17:22:22 -0300

    Shipping Class

--- vendor/hcodebr/php-classes/src/PagSeguro/Shipping.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Shipping.php
@@ -13,6 +13,53 @@ class Shipping {
 	private $cost;
 	private $addressRequired;
 
+	public function __construct(
+		Address $address,
+		int $type,
+		float $cost,
+		bool $addressRequired = true
+	)
+	{
+
+		if ($type < 1 || $type > 3) 
+		{
+
+			throw new Exception("Informe um tipo de frete válido.");
+			
+		}
+
+		$this->address = $address;
+		$this->type = $type;
+		$this->cost = $cost;
+		$this->addressRequired = $addressRequired;
+
+	}
+
+	public function getDOMElement():DOMElement
+	{
+
+		$dom = new DOMElement();
+
+		$shipping = $dom->createElement("shipping");
+		$shipping = $dom->appendChild($shipping);
+
+		$address = $this-$address->getDOMElement();
+		$address = $dom->importNode($address, true);
+		$address = $documents->appendChild($address);
+
+		$cost = $dom->createElement("cost", number_format($this->cost, 2, ".", ""));
+		$cost = $shipping->appendChild($cost);
+
+		$type = $dom->createElement("type", $this->type);
+		$type = $shipping->appendChild($type);
+
+		$addressRequired = $dom->createElement("addressRequired", if ($this->addressRequired) ? "true" : "false");
+		$addressRequired = $shipping->appendChild($addressRequired);
+
+		return $shipping;
+
+	}
+
 }
 
  ?>
